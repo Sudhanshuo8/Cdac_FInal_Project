2840c51406ee895acc122dad186cca84b96d0fd1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-14 23:07:00 +0800

    Delete BackEnd/Bachat-Gat5/Bachat-Gat5 directory

--- BackEnd/Bachat-Gat5/Bachat-Gat5/src/main/java/com/BachatGat/repo/BachatGatRepoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.BachatGat.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BachatGat.dto.GroupLeader;
-
-public interface BachatGatRepoInterface extends JpaRepository<GroupLeader, Long> {
-
-}
